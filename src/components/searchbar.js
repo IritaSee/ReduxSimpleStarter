e1fdfa9543b10a3313096ade33febdf3240c8fe9
Iga Narendra <iga.narendra@gmail.com>
2022-09-19 16:40:03 +0700

    track state

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React , {Component} from 'react';
 
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
 
-};
+        this.state = {term: ''};
+    }
+
+    render() {
+        // return <input onChange={this.onInputChange} />;
+        //or
+        //return <input onChange={(event) => console.log(event.target.value) } />;
+        return 
+        (
+            <div>
+            <input onChange={event => this.setState({term: event.target.value}) } />
+            Value of the input: {this.state.term}
+            </div>
+        );
+    }
+
+    // got commented because using the second variation
+    // onInputChange(event) {
+    //     //whenever the input changes, run these
+    //     //param: event = what is actually going on?
+    //     console.log(event.target.value);
+
+    // } 
+}
+
+new SearchBar;
 
 export default SearchBar;
